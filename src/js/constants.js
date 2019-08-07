c67f49d9bb909c162b765b598b9a3a1de5a7e499
Heath Carr <carrn7k@gmail.com>
2019-08-07 18:08:07 +0900

    fixed error with modal for new project

--- src/js/constants.js
+++ src/js/constants.js
@@ -92,7 +92,7 @@ const texts = {
 	 	text: `I don't have a lot of experience with d3, but I had a lot of fun building this app as a way to improve my knowledge. The app loads recent
 	 	 crime data and then visualizes that data using a histogram and a world map. Formatting the data and integrating it with the 
 	 	 visualization and transitions was a bit difficult at first, but I really enjoyed working with the d3 library.`
-	 }
+	 },
 
 	 reactComplete: {
 	 	title: 'Crown Live React App',
